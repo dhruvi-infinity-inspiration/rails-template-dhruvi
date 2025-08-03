5d4960e6cd048edd3bfa3243c1b02c21dc48f7f9
Maciej Sypniewski <msypniewski511@gmail.com>
2025-08-03 21:27:56 +0100

    Update application.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,28 +1,11 @@
 // app/javascript/controllers/application.js
 import { Application } from '@hotwired/stimulus'
-import StimulusReflex from 'stimulus_reflex'
 
-// Start Stimulus
 const application = Application.start()
 
-application.warnings = true
+// Configure Stimulus development experience
 application.debug = false
 window.Stimulus = application
-
-// Auto-register all controllers using import.meta.glob
-const controllers = import.meta.glob('./**/*_controller.js', { eager: true })
-
-for (const path in controllers) {
-  const controller = controllers[path].default
-  const identifier = path
-    .replace('./', '')
-    .replace('_controller.js', '')
-    .replace(/\//g, '--') // optional if nested folders
-  application.register(identifier, controller)
-}
-
-// Init StimulusReflex
-StimulusReflex.initialize(application, { isolate: true })
 
 export { application }
 
