03303bb333ac0b63d9399f2ad56ff12b483251eb
Maciej Sypniewski <msypniewski511@gmail.com>
2025-08-03 21:18:05 +0100

    Update application.js

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,20 +1,28 @@
 // app/javascript/controllers/application.js
 import { Application } from '@hotwired/stimulus'
 import StimulusReflex from 'stimulus_reflex'
-import { definitionsFromContext } from '@hotwired/stimulus-webpack-helpers'
 
+// Start Stimulus
 const application = Application.start()
 
-// Dev experience
 application.warnings = true
 application.debug = false
 window.Stimulus = application
 
-// Register all controllers automatically
-const context = require.context('.', true, /\.js$/)
-application.load(definitionsFromContext(context))
+// Auto-register all controllers using import.meta.glob
+const controllers = import.meta.glob('./**/*_controller.js', { eager: true })
 
-// Init StimulusReflex with isolation
+for (const path in controllers) {
+  const controller = controllers[path].default
+  const identifier = path
+    .replace('./', '')
+    .replace('_controller.js', '')
+    .replace(/\//g, '--') // optional if nested folders
+  application.register(identifier, controller)
+}
+
+// Init StimulusReflex
 StimulusReflex.initialize(application, { isolate: true })
 
 export { application }
+
